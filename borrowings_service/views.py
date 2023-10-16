eb0072dd07aded0a05d7a93eb81f25a979c19bef
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-10-16 13:56:14 +0300

    The books_service, users_service and corresponding API applications have been created

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.shortcuts import render
+# from rest_framework import mixins, viewsets
+#
+# from borrowings_service.models import Borrowing
+# from borrowings_service.serializers import BorrowingSerializer
+#
+#
+# class BorrowingViewSet(
+#     mixins.ListModelMixin,
+#     mixins.CreateModelMixin,
+#     mixins.RetrieveModelMixin,
+#     viewsets.GenericViewSet,
+# ):
+#     queryset = Borrowing.objects.select_related("books", "users")
+#     serializer_class = BorrowingSerializer
